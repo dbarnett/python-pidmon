7e45de9a6e5af848f8c682bb6f3f470112004632
David Barnett <davidbarnett2@gmail.com>
2011-06-04 20:26:53 -0400

    Test pidmon.waitpid on grandchild and sibling processes

--- pidmon/tests/_utils.py
+++ pidmon/tests/_utils.py
@@ -1,13 +1,24 @@
+import logging
+import os
 import multiprocessing
+import time
+
+import pidmon
+
+logger = logging.getLogger(__name__)
 
 def _run_DummyProcess(q):
     q.get()     # die as soon as something is put on queue
 
 class DummyProcess(object):
     """Throw-away process to wait idle until stopped"""
-    def __init__(self):
-        self.queue = multiprocessing.Queue()
-        self.process = multiprocessing.Process(target=_run_DummyProcess, args=(self.queue,))
+    def __init__(self, queue=None, target=_run_DummyProcess, args=None):
+        if queue is None:
+            queue = multiprocessing.Queue()
+        if args is None:
+            args = (queue,)
+        self.queue = queue
+        self.process = multiprocessing.Process(target=target, args=args)
 
     def start(self):
         self.process.start()
@@ -16,3 +27,15 @@ class DummyProcess(object):
         if self.process.is_alive():
             self.queue.put('STOP')
             self.process.join()
+
+def _run_SpawnOnceProcess(q):
+    p = DummyProcess(q)
+    p.start()
+    logger.info("Started child, PID {}".format(p.process.pid))
+    p.process.join()
+    logger.info("Child {} finished".format(p.process.pid))
+
+class SpawnOnceProcess(DummyProcess):
+    def __init__(self):
+        DummyProcess.__init__(self, target=_run_SpawnOnceProcess)
+
--- pidmon/tests/tests.py
+++ pidmon/tests/tests.py
@@ -4,7 +4,7 @@ import threading
 import time
 
 import pidmon
-from ._utils import DummyProcess
+from . import _utils
 
 class TestProcessObj(unittest.TestCase):
     def setUp(self):
@@ -31,7 +31,7 @@ class TestListProcesses(unittest.TestCase):
     def test_child_in_processes(self):
         """list_processes() should contain new process's PID"""
         running_pids_pre = [p.pid for p in pidmon.list_processes()]
-        p1 = DummyProcess()
+        p1 = _utils.DummyProcess()
         try:
             p1.start()
             while p1.process.pid is None:        # make sure it's started
@@ -58,7 +58,7 @@ class TestMonitor(unittest.TestCase):
 
     def test_spawn(self):
         self.monitor.poll_procs()
-        p1 = DummyProcess()
+        p1 = _utils.DummyProcess()
         try:
             p1.start()
             while p1.process.pid is None:        # make sure it's started
@@ -105,13 +105,47 @@ class TestWaitPID(unittest.TestCase):
         self.assertLess(time_taken, .5)
 
     def test_childpid(self, delay=.05):
-        """Handles direct child (nothing os.waitpid can't do)"""
-        p1 = DummyProcess()
+        """waitpid handles direct child (nothing os.waitpid can't do)"""
+        p1 = _utils.DummyProcess()
         try:
             p1.start()
             self._stop_process_after(p1, delay)
             t0 = time.time()
             pidmon.waitpid(p1.process.pid)
+        finally:
+            p1.stop()
+        t1 = time.time()
+        assert (delay < t1 - t0), "died {:.2f}s into {:.2f}s delay".format(t1 - t0, delay)
+
+    def test_grandchildpid(self, delay=.05):
+        """waitpid handles grandchild (os.waitpid can't do this)"""
+        p1 = _utils.SpawnOnceProcess()
+        try:
+            p1.start()
+            p1_handle = pidmon.Process(p1.process.pid)
+            (p2_handle,) = p1_handle.children
+            self._stop_process_after(p1, delay)
+            t0 = time.time()
+            pidmon.waitpid(p1.process.pid)
+        finally:
+            p1.stop()
+        t1 = time.time()
+        assert (delay < t1 - t0), "died {:.2f}s into {:.2f}s delay".format(t1 - t0, delay)
+
+    def test_siblingpid(self, delay=.05):
+        """waitpid handles sibling process (os.waitpid can't do this)"""
+        p1 = _utils.DummyProcess()
+        try:
+            p1.start()
+            # Note: in this test, waitpid is called in the child process
+            p2 = _utils.DummyProcess(target=pidmon.waitpid, args=(p1.process.pid,))
+            try:
+                p2.start()
+                self._stop_process_after(p1, delay)
+                t0 = time.time()
+                p2.process.join()
+            finally:
+                p2.stop()
         finally:
             p1.stop()
         t1 = time.time()
